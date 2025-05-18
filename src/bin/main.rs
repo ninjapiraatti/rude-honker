a01ba631f3f0a08fe2dd32f89e59fb6a2af62bc8
ninjapiraatti <tuoppil@gmail.com>
2025-05-18 08:10:21 +0300

    Make motors run

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -44,36 +44,36 @@ async fn main(spawner: Spawner) {
     .unwrap();
 
     // Motors
-    let motor_a1_pin1 = Output::new(peripherals.GPIO22, Level::Low, OutputConfig::default());
-    let motor_a1_pin2 = Output::new(peripherals.GPIO23, Level::Low, OutputConfig::default());
+    let motor_a1_pin1 = Output::new(peripherals.GPIO23, Level::Low, OutputConfig::default());
+    let motor_a1_pin2 = Output::new(peripherals.GPIO22, Level::Low, OutputConfig::default());
     let motor_a2_pin1 = Output::new(peripherals.GPIO21, Level::Low, OutputConfig::default());
     let motor_a2_pin2 = Output::new(peripherals.GPIO20, Level::Low, OutputConfig::default());
     let motor_b1_pin1 = Output::new(peripherals.GPIO9, Level::Low, OutputConfig::default());
-    let motor_b1_pin2 = Output::new(peripherals.GPIO12, Level::Low, OutputConfig::default());
+    let motor_b1_pin2 = Output::new(peripherals.GPIO13, Level::Low, OutputConfig::default());
     let motor_b2_pin1 = Output::new(peripherals.GPIO18, Level::Low, OutputConfig::default());
-    let motor_b2_pin2 = Output::new(peripherals.GPIO2, Level::Low, OutputConfig::default());
+    let motor_b2_pin2 = Output::new(peripherals.GPIO3, Level::Low, OutputConfig::default());
     let mut stdby_pin = Output::new(peripherals.GPIO11, Level::Low, OutputConfig::default());
 
     let motor_a1_pwm_pin = peripherals.GPIO15;
     let motor_a2_pwm_pin = peripherals.GPIO19;
-    let motor_b1_pwm_pin = peripherals.GPIO13;
-    let motor_b2_pwm_pin = peripherals.GPIO10;
+    let motor_b1_pwm_pin = peripherals.GPIO12;
+    let motor_b2_pwm_pin = peripherals.GPIO2;
 
     let clock_cfg = PeripheralClockConfig::with_frequency(Rate::from_mhz(40)).unwrap();
     let mut mcpwm_a = McPwm::new(peripherals.MCPWM0, clock_cfg);
-    //let mut mcpwm_b = McPwm::new(MCPWM1, clock_cfg);
+    //let mut mcpwm_b = McPwm::new(peripherals.MCPWM1, clock_cfg);
     mcpwm_a.operator0.set_timer(&mcpwm_a.timer0);
     mcpwm_a.operator1.set_timer(&mcpwm_a.timer1);
     //mcpwm_b.operator0.set_timer(&mcpwm_b.timer0);
     //mcpwm_b.operator1.set_timer(&mcpwm_b.timer1);
-    let motor_a1_pwm = mcpwm_a.operator0.with_pin_a(motor_a1_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
-    let motor_a2_pwm = mcpwm_a.operator1.with_pin_a(motor_a2_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
-    //let motor_b1_pwm = mcpwm_b.operator0.with_pin_a(motor_b1_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
-    //let motor_b2_pwm = mcpwm_b.operator1.with_pin_a(motor_b2_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
-    let mut motor_a1 = Motor::new(motor_a1_pin1, motor_a1_pin2, motor_a1_pwm).unwrap();
-    let mut motor_a2 = Motor::new(motor_a2_pin1, motor_a2_pin2, motor_a2_pwm).unwrap();
-    //let mut motor_b1 = Motor::new(motor_b1_pin1, motor_b1_pin2, motor_b1_pwm).unwrap();
-    //let mut motor_b2 = Motor::new(motor_b2_pin1, motor_b2_pin2, motor_b2_pwm).unwrap();
+    let motor_a1_pwm = mcpwm_a.operator0.with_pins(motor_a1_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH, motor_a2_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
+    //let motor_a2_pwm = mcpwm_a.operator0.with_pin_b(motor_a2_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
+    let motor_b1_pwm = mcpwm_a.operator1.with_pins(motor_b1_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH, motor_b2_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
+    //let motor_b2_pwm = mcpwm_a.operator1.with_pin_b(motor_b2_pwm_pin, PwmPinConfig::UP_ACTIVE_HIGH);
+    let mut motor_a1 = Motor::new(motor_a1_pin1, motor_a1_pin2, motor_a1_pwm.0).unwrap();
+    let mut motor_a2 = Motor::new(motor_a2_pin1, motor_a2_pin2, motor_a1_pwm.1).unwrap();
+    let mut motor_b1 = Motor::new(motor_b1_pin1, motor_b1_pin2, motor_b1_pwm.0).unwrap();
+    let mut motor_b2 = Motor::new(motor_b2_pin1, motor_b2_pin2, motor_b1_pwm.1).unwrap();
 
     let timer_clock_cfg = clock_cfg
         .timer_clock_with_frequency(99, PwmWorkingMode::Increase, Rate::from_khz(20))
@@ -93,31 +93,113 @@ async fn main(spawner: Spawner) {
         let interval = 1000;
         let speed = 100;
 
+        motor_a1.drive(DriveCommand::Stop).expect("driving");
+        motor_a2.drive(DriveCommand::Stop).expect("driving");
+        motor_b1.drive(DriveCommand::Stop).expect("driving");
+        motor_b2.drive(DriveCommand::Stop).expect("driving");
+        delay.delay_millis(interval);
+
         motor_a1.drive(DriveCommand::Forward(speed)).expect("driving");
-        println!("Debug: {:?}", motor_a1.current_drive_command());
+        println!("A1: {:?}", motor_a1.current_drive_command());
         delay.delay_millis(interval);
         motor_a1.drive(DriveCommand::Backward(speed)).expect("driving");
+        println!("A1: {:?}", motor_a1.current_drive_command());
         delay.delay_millis(interval);
+        motor_a1.drive(DriveCommand::Stop).expect("driving");
 
         motor_a2.drive(DriveCommand::Forward(speed)).expect("driving");
-        println!("Debug: {:?}", motor_a2.current_drive_command());
+        println!("A2: {:?}", motor_a2.current_drive_command());
         delay.delay_millis(interval);
         motor_a2.drive(DriveCommand::Backward(speed)).expect("driving");
+        println!("A2: {:?}", motor_a2.current_drive_command());
         delay.delay_millis(interval);
+        motor_a2.drive(DriveCommand::Stop).expect("driving");
 
-        /*
         motor_b1.drive(DriveCommand::Forward(speed)).expect("driving");
-        println!("Debug: {:?}", motor_b1.current_drive_command());
-        delay.delay_millis(500);
+        println!("B1: {:?}", motor_b1.current_drive_command());
+        delay.delay_millis(interval);
         motor_b1.drive(DriveCommand::Backward(speed)).expect("driving");
-        delay.delay_millis(500);
+        println!("B1: {:?}", motor_b1.current_drive_command());
+        delay.delay_millis(interval);
+        motor_b1.drive(DriveCommand::Stop).expect("driving");
 
         motor_b2.drive(DriveCommand::Forward(speed)).expect("driving");
-        println!("Debug: {:?}", motor_b2.current_drive_command());
-        delay.delay_millis(500);
+        println!("B2: {:?}", motor_b2.current_drive_command());
+        delay.delay_millis(interval);
         motor_b2.drive(DriveCommand::Backward(speed)).expect("driving");
-        delay.delay_millis(500);
-        */
+        println!("B2: {:?}", motor_b2.current_drive_command());
+        delay.delay_millis(interval);
+        motor_b2.drive(DriveCommand::Stop).expect("driving");
+
+        println!("Test patterns");
+        motor_a1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_a2.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_b1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_b2.drive(DriveCommand::Forward(speed)).expect("driving");
+
+        delay.delay_millis(interval);
+
+        motor_a1.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_a2.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_b1.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_b2.drive(DriveCommand::Backward(speed)).expect("driving");
+
+        delay.delay_millis(interval);
+        motor_a1.drive(DriveCommand::Stop).expect("driving");
+        motor_a2.drive(DriveCommand::Stop).expect("driving");
+        motor_b1.drive(DriveCommand::Stop).expect("driving");
+        motor_b2.drive(DriveCommand::Stop).expect("driving");
+
+        motor_a1.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_a2.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_b1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_b2.drive(DriveCommand::Backward(speed)).expect("driving");
+
+        delay.delay_millis(interval);
+        motor_a1.drive(DriveCommand::Stop).expect("driving");
+        motor_a2.drive(DriveCommand::Stop).expect("driving");
+        motor_b1.drive(DriveCommand::Stop).expect("driving");
+        motor_b2.drive(DriveCommand::Stop).expect("driving");
+
+        motor_a1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_a2.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_b1.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_b2.drive(DriveCommand::Forward(speed)).expect("driving");
+
+        delay.delay_millis(interval);
+        motor_a1.drive(DriveCommand::Stop).expect("driving");
+        motor_a2.drive(DriveCommand::Stop).expect("driving");
+        motor_b1.drive(DriveCommand::Stop).expect("driving");
+        motor_b2.drive(DriveCommand::Stop).expect("driving");
+
+        motor_a1.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_a2.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_b1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_b2.drive(DriveCommand::Forward(speed)).expect("driving");
+
+        delay.delay_millis(interval);
+        motor_a1.drive(DriveCommand::Stop).expect("driving");
+        motor_a2.drive(DriveCommand::Stop).expect("driving");
+        motor_b1.drive(DriveCommand::Stop).expect("driving");
+        motor_b2.drive(DriveCommand::Stop).expect("driving");
+
+        motor_b1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_b2.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_a1.drive(DriveCommand::Backward(speed)).expect("driving");
+        motor_a2.drive(DriveCommand::Backward(speed)).expect("driving");
+
+        delay.delay_millis(interval);
+        motor_a1.drive(DriveCommand::Stop).expect("driving");
+        motor_a2.drive(DriveCommand::Stop).expect("driving");
+        motor_b1.drive(DriveCommand::Stop).expect("driving");
+        motor_b2.drive(DriveCommand::Stop).expect("driving");
+
+        motor_a1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_a2.drive(DriveCommand::Forward(speed / 2)).expect("driving");
+        motor_b1.drive(DriveCommand::Forward(speed)).expect("driving");
+        motor_b2.drive(DriveCommand::Forward(speed / 2)).expect("driving");
+
+        delay.delay_millis(interval);
     }
 
     // for inspiration have a look at the examples at https://github.com/esp-rs/esp-hal/tree/esp-hal-v1.0.0-beta.0/examples/src/bin
